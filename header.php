5348bde3578b34940bc58508311bb451913dc4d3
jobin <jobin@webeteer.com>
2016-09-03 09:07:11 +0530

    Included bootstrap and basic files

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php 
+require_once('config.php') ;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Right Choice</title>
+<link rel="stylesheet" href="<?php echo ROOT_DIR ?>css/style.css" />
+<link rel="stylesheet" href="<?php echo ROOT_DIR ?>css/layout.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="shortcut icon" type="image/png" href="<?php echo ROOT_DIR ?>images/fav.png"/>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
